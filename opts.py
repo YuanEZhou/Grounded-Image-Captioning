044685a0fb049b7d49a930bb3defc3bc92ecf363
YE Zhou <1977212346@qq.com>
2022-01-05 20:47:01 +0800

    Update opts.py

--- opts.py
+++ opts.py
@@ -37,10 +37,10 @@ def parse_opt():
                     help='path to the directory containing the boxes of att feats')
     parser.add_argument('--input_label_h5', type=str, default='data/flickrtalk_label.h5',
                     help='path to the h5file containing the preprocessed dataset')
-    parser.add_argument('--cached_tokens', type=str, default='data/flickr-train-idxs',
+    parser.add_argument('--cached_tokens', type=str, default='flickr-train-idxs',
                     help='Cached token file for calculating cider score during self critical training.')
 
-    parser.add_argument('--start_from', type=str, default='./log/CE-gt-sup-0.1-nll',
+    parser.add_argument('--start_from', type=str, default= None,
                     help="""continue training from saved model at this path. Path must contain files saved by previous training process: 
                         'infos.pkl'         : configuration;
                         'checkpoint'        : paths to model file(s) (created by tf).
